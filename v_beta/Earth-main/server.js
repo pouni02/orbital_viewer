b938b8caa00dccb4034fef05f124934ddacf2ddc
pouni02 <pounida.nadaradjane@gmail.com>
2025-10-05 08:15:01 +0000

    init

--- v_beta/Earth-main/server.js
+++ v_beta/Earth-main/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Sert tout le dossier public à la racine (textures, index.html, etc.)
-app.use(express.static(__dirname + '/public'));
+// Sert tout le dossier public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Sert Three.js
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+
+// Forcer la racine à renvoyer index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(3000, () =>
   console.log('Visit http://127.0.0.1:3000')
